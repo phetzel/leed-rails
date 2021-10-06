779411ee50f4b65a9ac22e60d59c86948c16091c
Phil <phetzel89@gmail.com>
2021-10-06 00:45:37 -0700

    User and session model and controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:name],
+            params[:user][:password]
+        )
+
+        if @user 
+            login!(@user)
+            render @current_user
+        else 
+            render json: ["Invalid email/password combination"], status: 401
+        end
+    end
+
+    def destroy
+        user = current_user
+        if user
+            logout!
+            render json["logged out"]
+        else
+            render json: ["Nobody signed in"], status: 404
+        end
+    end
+end
