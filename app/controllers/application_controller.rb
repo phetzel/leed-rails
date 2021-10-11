e0cfdee942d63ba1055f37375cc13fa636f86298
Phil <phetzel89@gmail.com>
2021-10-10 19:56:13 -0700

    Cors fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
     # helper_method :current_user, :logged_in?
     # skip_forgery_protection
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, raise: false
     include ActionController::Helpers
 
 
