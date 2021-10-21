90036c5a558e39890f8595922f1bdff52e3db50a
Phil <phetzel89@gmail.com>
2021-10-20 18:00:26 -0700

    Domain cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,4 +12,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: %i(get post put patch delete options head)
   end
+  allow do
+    origins 'leed-next.vercel.app'
+    resource '*',
+      headers: :any,
+      methods: %i(get post put patch delete options head)
+  end
 end
